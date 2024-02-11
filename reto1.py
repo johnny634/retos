29bc3adaf1f4d2d010d9dae570b443ad32b357b9
johnny634 <126592204+johnny634@users.noreply.github.com>
2024-02-11 14:25:51 -0500

    Se agrega el proyecto.

---
+++ reto1.py
@@ -0,0 +1,9 @@
+# Reto de la semana
+
+saludo    =   'Hola'
+nombre    =    input('Ingrese su nombre: ')
+apellido  =    input('Ingrese su apellido: ')
+telefono  =    int(input('Ingrese su telefono: '))
+correo    =    input ('Ingrese su correo electronico: ')
+
+print(saludo + ", Mi nombre es " + nombre + ",mi apellido es " + apellido + ",mi telefono es " + str(telefono) + ", mi correo es " + correo)
